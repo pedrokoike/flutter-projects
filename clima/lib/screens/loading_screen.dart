c9b45343cc67703ddc2ab2af8be4d8471205cd52
pedrokoike <pedro.koike@gmail.com>
2022-04-18 16:38:04 -0300

    Clima app added

---
+++ clima/lib/screens/loading_screen.dart
@@ -0,0 +1,50 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(weatherData);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Ana Carolina',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SpinKitDoubleBounce(
+              color: Colors.white,
+              size: 100.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ clima/lib/services/location.dart
@@ -0,0 +1,17 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude = 0.0;
+  double longitude = 0.0;
+  Future<void> getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
